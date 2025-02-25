9a8b15b2cf7329b7ea823aabcb78374d5036caf6
Pragati <pragati.sri2411@gmail.com>
2025-02-25 22:37:37 +0530

    added annotations for failed test case

--- src/test/java/com/expense/management/ExpenseManagementApplicationTests.java
+++ src/test/java/com/expense/management/ExpenseManagementApplicationTests.java
@@ -1,9 +1,13 @@
 package com.expense.management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+@SpringBootTest(classes = ExpenseManagementApplication.class)
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.expense.management")
 class ExpenseManagementApplicationTests {
 
 	@Test
